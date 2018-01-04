67562953460cc9b0716267a7ce1d08597a0bd27e
TobiasSlott <tobias.slott@post.tele.dk>
2018-01-04 14:12:46 +0100

    Added matmult_lib.c

---
+++ matmult_lib.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <cblas.h>
+
+// Declarations
+void matmult_lib(int m,int n,int k,double *A,double *B,double *C);
+
+void matmult_lib(int m,int n,int k,double *A,double *B,double *C) {
+  int alpha = 1, beta = 0;
+  int lda = k, ldb = n, ldc = n;
+
+  for (i=0; i<m*n; i++) C[i] = 0;
+
+  cblas_dgemm(CblasRowMajor,  CblasNoTrans, CblasNoTrans, m, n, k,
+    alpha, A, lda, B, ldb, beta, C, ldc);
+}
